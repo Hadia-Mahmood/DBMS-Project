0ff3f2350e40069e60b982ad028260066afc77e8
hadia <hadiamahmood4420@gmail.com>
2023-07-14 04:56:46 +0500

    resolved errors

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,16 +1,1 @@
-var dropdown = document.getElementById("myDropdown");
-var input = document.getElementById("selectedValueInput");
-
-// Add event listener for when the dropdown value changes
-dropdown.addEventListener("change", function() {
-  // Get the selected value
-  var selectedValue = dropdown.value;
-  
-  // Update the input field with the selected value
-  input.value = selectedValue;
-});
-
-
-
-
 
