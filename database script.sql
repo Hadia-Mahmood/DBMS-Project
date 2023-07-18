e6ac7cb89f722e698b3ef0e4aeec34d8c5d7427c
hadia <hadiamahmood4420@gmail.com>
2023-07-18 23:42:55 +0500

    script file attached

--- database script.sql
+++ database script.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `airline_reservation_system` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `airline_reservation_system`;
 -- MySQL dump 10.13  Distrib 8.0.33, for Win64 (x86_64)
 --
 -- Host: localhost    Database: airline_reservation_system
@@ -469,4 +471,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-07-18 22:44:24
+-- Dump completed on 2023-07-18 23:39:12
